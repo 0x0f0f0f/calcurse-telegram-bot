27c899c502d4a684d782e9294fb0c1c6a1570368
a <a@a.a>
2019-08-18 22:16:07 +0200

    now pulls repo and reads file from repo

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const dotenv = require('dotenv')
 const TelegramBot = require('node-telegram-bot-api')
 const fs = require('fs')
 const path = require('path')
+const { exec } = require('child_process')
+
 
 // Read env variables from .env
 dotenv.config()
 const token = process.env.TOKEN
 const privchatid = process.env.PRIVATECHATID
 const calcursepath = process.env.CALCURSEPATH
+const gitrepourl = process.env.GITREPOURL
 
 const bot = new TelegramBot(token, {polling: true})
 
@@ -20,17 +23,14 @@ const completedTodoRegexp = /^\[(?<priority>\-\d)\](?<notehash>>[\dA-Za-z]+)*\s(
 
 let todos = []
 
-// TODO read file from git repo instead of fs
-// TODO setTimeout parse from git every 5000ms
 // TODO observable array notify on new
 // TODO create new todo
 // TODO read note from todo
 
-
 const parseTodoFile = async () => {
     let cnt;
     try {
-        cnt = fs.readFileSync(path.join(calcursepath, 'todo'), 'utf8')
+        cnt = fs.readFileSync(path.join(__dirname, 'tmp', calcursepath, 'todo'), 'utf8')
     } catch (err) {
         console.error(err)
         bot.sendMessage(privchatid, "ERROR: Could not read todo file")
@@ -62,7 +62,33 @@ const buildTodoString = (todos, completed) => {
     return str
 }
 
-parseTodoFile()
+const updateThenParse = () => {
+    if(fs.existsSync('./tmp')) {
+        exec(`git pull`, {cwd: path.join(__dirname, 'tmp')}, (err, stdout, stderr) => {
+            console.error(stderr)
+            if(err) {
+                console.error(`Error while pulling git repo: ${err.code}`)
+                bot.sendMessage(privchatid, "ERROR: Could not pull git repo")
+            }
+            console.log('pulled git repo')
+            parseTodoFile()
+        })
+    } else {
+        exec(`git clone ${gitrepourl} ./tmp`, (err, stdout, stderr) => {
+            if(err) {
+                console.error(`Error while cloning git repo: ${err.code}`)
+                bot.sendMessage(privchatid, "ERROR: Could not clone git repo")
+            }
+            console.log('cloned git repo')
+            parseTodoFile()
+        })
+    }
+}
+
+
+updateThenParse()
+setTimeout(updateThenParse, 20000)
+
 
 // echo
 bot.onText(/\/echo (.+)/, (msg, match) => {
