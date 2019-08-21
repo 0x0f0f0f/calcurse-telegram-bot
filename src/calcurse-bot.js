b0f9bd17030bdae24f3c0b07daa1e85f791478ca
a <a@a.a>
2019-08-21 15:50:02 +0200

    airbnb standard compliance and diff parsing module

--- src/calcurse-bot.js
+++ src/calcurse-bot.js
@@ -1,99 +1,99 @@
+/* eslint-disable eqeqeq */
 
-const TelegramBot = require('node-telegram-bot-api')
-const conf = require('../config')
-const parse = require('./parse')
+import TelegramBot from 'node-telegram-bot-api';
+import conf from '../config';
+import { gitUpdate, parseTodoFile } from './parse';
 
 // Create the bot
-const bot = new TelegramBot(conf.token, {polling: true})
+const bot = new TelegramBot(conf.token, { polling: true });
 
 // TODO create new todo
 // TODO read note from todo
 
 const buildTodoString = (todos, completed) => {
-    let str = completed ? "COMPLETED\n" : "ACTIVE:\n"
-    let count = 0
-    if(!todos) return "ERROR Parsing"
-    todos.filter(todo => completed == todo.completed).forEach(todo => {
-        str += `${count}: ` + (completed ? "" : `(${todo.priority}) `) + `${todo.text}\n`
-        count++
-    })
-    return str
-}
+  let str = completed ? 'COMPLETED\n' : 'ACTIVE:\n';
+  let count = 0;
+  if (!todos) return 'ERROR Parsing';
+  todos.filter((todo) => completed == todo.completed).forEach((todo) => {
+    str += `${count}: ${completed ? '' : `(${todo.priority})`} ${todo.text}\n`;
+    count += 1;
+  });
+  return str;
+};
 
 // Listen on sigpipe to parse the diff and detect new/deleted/completed todos
 process.on('SIGPIPE', async (signal) => {
-    let sent_msg = await bot.sendMessage(conf.privchatid, `Received ${signal}`)
+  const sentMsg = await bot.sendMessage(conf.privchatid, `Received ${signal}`);
 
-    await bot.editMessageText("Retrieving TODO file from git", {
-        chat_id: msg.chat.id,
-        message_id: sent_msg.message_id
-    })
+  await bot.editMessageText('Retrieving TODO file from git', {
+    chat_id: conf.conf.privchatid,
+    message_id: sentMsg.message_id,
+  });
 
-    try {
-        await parse.gitUpdate()
-    } catch (error) {
-        console.error(error)
-        await bot.editMessageText(`Error: ${error}`, {
-            chat_id: msg.chat.id,
-            message_id: sent_msg.message_id
-        })
-    }
+  try {
+    await gitUpdate();
+  } catch (error) {
+    console.error(error);
+    await bot.editMessageText(`Error: ${error}`, {
+      chat_id: conf.privchatid,
+      message_id: sentMsg.message_id,
+    });
+  }
 
-    //TODO parse diff
-    await bot.editMessageText("Parsing TODO file diff with last commit", {
-        chat_id: msg.chat.id,
-        message_id: sent_msg.message_id
-    })
-})
+  // TODO parse diff
+  await bot.editMessageText('Parsing TODO file diff with last commit', {
+    chat_id: conf.privchatid,
+    message_id: sentMsg.message_id,
+  });
+});
 
 // echo
 bot.onText(/\/echo (.+)/, (msg, match) => {
-    if(msg.chat.id != conf.privchatid) return
-    bot.sendMessage(msg.chat.id, match[1])
-})
+  if (msg.chat.id != conf.privchatid) return;
+  bot.sendMessage(msg.chat.id, match[1]);
+});
 
 // Get a list of active todos
 bot.onText(/\/(active|completed)/, async (msg, match) => {
-    console.dir(match[1])
-    if(msg.chat.id != conf.privchatid) return
-    let sent_msg = await bot.sendMessage(msg.chat.id, "Retrieving TODO file from git", { 
-        reply_to_message_id: msg.message_id
-    })
+  console.dir(match[1]);
+  if (msg.chat.id != conf.privchatid) return;
+  const sentMsg = await bot.sendMessage(msg.chat.id, 'Retrieving TODO file from git', {
+    reply_to_message_id: msg.message_id,
+  });
 
-    try {
-        await parse.gitUpdate()
-    } catch (error) {
-        console.error(error)
-        await bot.editMessageText(`Error: ${error}`, {
-            chat_id: msg.chat.id,
-            message_id: sent_msg.message_id
-        })
-    }
+  try {
+    await gitUpdate();
+  } catch (error) {
+    console.error(error);
+    await bot.editMessageText(`Error: ${error}`, {
+      chat_id: msg.chat.id,
+      message_id: sentMsg.message_id,
+    });
+  }
 
-    await bot.editMessageText("Parsing TODO file", {
-        chat_id: msg.chat.id,
-        message_id: sent_msg.message_id
-    })
+  await bot.editMessageText('Parsing TODO file', {
+    chat_id: msg.chat.id,
+    message_id: sentMsg.message_id,
+  });
 
-    let todos 
-    try {
-        todos = await parse.parseTodoFile()
-    } catch (error) {
-        await bot.editMessageText(`Error: ${error}`, {
-            chat_id: msg.chat.id,
-            message_id: sent_msg.message_id
-        })
-    }
+  let todos;
+  try {
+    todos = await parseTodoFile();
+  } catch (error) {
+    await bot.editMessageText(`Error: ${error}`, {
+      chat_id: msg.chat.id,
+      message_id: sentMsg.message_id,
+    });
+  }
 
-    await bot.editMessageText(buildTodoString(todos, match[1] == 'completed'), {
-        chat_id: msg.chat.id,
-        message_id: sent_msg.message_id
-    })
-    
-})
+  await bot.editMessageText(buildTodoString(todos, match[1] == 'completed'), {
+    chat_id: msg.chat.id,
+    message_id: sentMsg.message_id,
+  });
+});
 
 // TODO mark todo as completed
 bot.onText(/\/mark (\d+)/, (msg, match) => {
-    if(msg.chat.id != conf.privchatid) return
-    bot.sendMessage(msg.chat.id, "Not yet implemented")
-})
+  if (msg.chat.id != conf.conf.privchatid) return;
+  bot.sendMessage(msg.chat.id, 'Not yet implemented');
+});
--- src/parse.js
+++ src/parse.js
@@ -1,64 +1,61 @@
-const util = require('util')
-const fs = require('fs')
-const path = require('path')
-const exec = util.promisify(require('child_process').exec)
-const crypto = require('crypto')
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
+const exec = util.promisify(require('child_process').exec);
+const crypto = require('crypto');
 
-const conf = require('../config')
+const conf = require('../config');
 
 // Parsing regexes
 // Parse an active` todo like:
 // [2]>13872a7f716e76 Todo text
 // first group is priority, second is note hash, third is text
-const todoRegexp = /^\[(?<priority>(?<completed>\-)*\d)\](?<notehash>>[\dA-Za-z]+)*\s(?<text>.*)$/
-const completedTodoRegexp = /^\[(?<priority>\-\d)\](?<notehash>>[\dA-Za-z]+)*\s(?<text>.*)$/
-
+const todoRegexp = /^\[(?<priority>(?<completed>-)*\d)\](?<notehash>>[\dA-Za-z]+)*\s(?<text>.*)$/;
 
 async function gitUpdate() {
-    if(fs.existsSync(conf.tmpdirpath)) {
-        const { stdout, stderr } = await exec(`git pull`, {cwd: conf.tmpdirpath})
-        if(stderr) {
-            console.error(stderr)
-            throw new Error(`Error while pulling git repo`)
-        }
-        console.log('Successfully pulled git repo')
-        return 
-    } else {
-        const { stdout, stderr } = await  exec(`git clone ${conf.gitrepourl} ${conf.tmpdirpath}`)
-        if(stderr) {
-            console.error(stderr)
-            throw new Error(`Error while cloning git repo`)
-        }
-        console.log('Successfully cloned git repo')
-        return
+  if (fs.existsSync(conf.tmpdirpath)) {
+    const { stderr } = await exec('git pull', { cwd: conf.tmpdirpath });
+    if (stderr) {
+      console.error(stderr);
+      throw new Error('Error while pulling git repo');
     }
+    console.log('Successfully pulled git repo');
+  } else {
+    const { stderr } = await exec(`git clone ${conf.gitrepourl} ${conf.tmpdirpath}`);
+    if (stderr) {
+      console.error(stderr);
+      throw new Error('Error while cloning git repo');
+    }
+    console.log('Successfully cloned git repo');
+  }
 }
 
 
-async function parseTodoFile () {
-    let cnt
-    let todos = new Map()
-    
-    cnt = await fs.promises.readFile(path.join(conf.tmpdirpath, conf.calcursepath, 'todo'), 'utf8')
-    cnt.split("\n").forEach(todostr => {
-        // Match if the current todo is active
-        let m = todostr.match(todoRegexp)
-        if (m != null) {
-            let t = {
-                priority: parseInt(m.groups.priority),
-                completed: m.groups.completed == "-" ? true : false,
-                notehash: m.groups.notehash ? m.groups.notehash.slice(1) : null,
-                text: m.groups.text
-            }
-            let key = crypto.createHash('sha256').update(JSON.stringify(t)).digest('hex')
-            todos.set(key, t)
-        }
-    });
-    console.log("Successfully parsed todo list")
-    return Array.from(todos.values())
+async function parseTodoFile() {
+  const todos = new Map();
+
+  const cnt = await fs.promises.readFile(path.join(conf.tmpdirpath, conf.calcursepath, 'todo'), 'utf8');
+  cnt.split('\n').forEach((todostr) => {
+    // Match if the current todo is active
+    const m = todostr.match(todoRegexp);
+    if (m != null) {
+      const t = {
+        priority: parseInt(m.groups.priority, 10),
+        completed: m.groups.completed === '-',
+        notehash: m.groups.notehash ? m.groups.notehash.slice(1) : null,
+        text: m.groups.text,
+      };
+      const key = crypto.createHash('sha256').update(JSON.stringify(t)).digest('hex');
+      todos.set(key, t);
+    }
+  });
+  console.log('Successfully parsed todo list');
+  return Array.from(todos.values());
 }
 
 
 module.exports = {
-    gitUpdate,  parseTodoFile
-}
+  gitUpdate, parseTodoFile,
+};
+
+'git --no-pager diff -p --no-color --raw HEAD^^ -- calcurse/.calcurse/todo calcurse/.calcurse/todo';
